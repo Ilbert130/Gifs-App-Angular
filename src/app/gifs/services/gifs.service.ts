89cd0b2108bd5f29d3d26770574007a17fcc0eff
Ilbert130 <ilbertca27@gmail.com>
2023-07-05 15:15:06 -0400

    using api

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gisf.interfaces';
+
+// const GIFS_API_KEY = '0M6BF5dXf4dVjB2Fydz9E4UF5qyAlHU9';
 
 @Injectable({
   // Para que sea globar y no haya que colocarlo como un provider
@@ -7,14 +11,43 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory:string[]=[];
+  public gifList:Gif[] =[]
+  private apiKey:string = '0M6BF5dXf4dVjB2Fydz9E4UF5qyAlHU9';
+  private serviceUrl:string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  public searchTag(tag:string):void{
-   this._tagsHistory.unshift(tag);
+  public searchTag(tag:string){
+    if(tag.length===0) return;
+    this.organizeHistory(tag);
+
+    // Tambien con este podemos setiar los parametros de la peticion
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag);
+
+    // Observable
+    // Nos suscribimos ahi ya tenemos la respuesta
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
+      .subscribe(resp => {
+        this.gifList = resp.data;
+      });
+
+  }
+
+  private organizeHistory(tag:string){
+    tag = tag.toLocaleLowerCase();
+
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0,10);
   }
 }
